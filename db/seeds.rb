9b60dbd84e55fc47fb0ca11ac1a21891db8f06fe
tuantranpham204 <12a1tranphamtuan@gmail.com>
2026-01-28 13:39:06 +0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+admin = User.create!(
+  username: "urfavadmin",
+  email: "admin@example.com",
+  password: "password",
+  password_confirmation: "password",
+  created_at: Time.now,
+  updated_at: Time.now,
+  confirmed_at: Time.now
+)
+admin.roles << User::ROLES[:ADMIN]
+
+20.times do |i|
+  User.create!(
+    username: "urfavuser#{i+1}",
+    email: "user#{i+1}@example.com",
+    password: "password",
+    password_confirmation: "password",
+    created_at: Time.now,
+    updated_at: Time.now,
+    confirmed_at: Time.now
+  )
+end
